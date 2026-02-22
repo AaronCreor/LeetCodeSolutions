0c007dfc3fe444c478c0234ffc202c568c2b3c41
Aaron <aaronmacleod123@gmail.com>
2026-02-21 22:20:51 -0800

    Time: 0 ms (100%), Space: 20.1 MB (9.94%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        pointer = 0
+        while left <= right:
+            pointer = (left+right) // 2
+            if (nums[pointer] == target):
+                return pointer
+            if (nums[pointer] > target):
+                right = pointer-1
+            else:
+                left = pointer+1
+        if(target > nums[pointer]):
+            return pointer+1
+        else:
+            return pointer
